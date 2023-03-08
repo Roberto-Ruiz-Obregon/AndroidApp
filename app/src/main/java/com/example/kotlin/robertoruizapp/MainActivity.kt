bb453a7ba15d42a27c115da786688978bdf33ffd
DenisseDB <A01702603@tec.mx>
2023-03-08 16:48:38 -0600

    inicio

--- app/src/main/java/com/example/kotlin/robertoruizapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/robertoruizapp/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.kotlin.robertoruizapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
 import com.example.kotlin.robertoruizapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: Activity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragmento_bienvenida
-        )
+
+        initializeBinding()
     }
 
-} 
+    private fun initializeBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
