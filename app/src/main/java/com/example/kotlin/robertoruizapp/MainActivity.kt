4c9d7a72b3e03feae068de202d1717e252c8c773
amatt2b <a01703889@tec.mx>
2023-03-08 21:32:47 -0600

    RecordTextInput

--- app/src/main/java/com/example/kotlin/robertoruizapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/robertoruizapp/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.example.kotlin.robertoruizapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
 import com.example.kotlin.robertoruizapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: Activity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
     }
+
 
 }
