b5a6931b4dfa1d74c406a1ea816ef4338ceaf827
amatt2b <a01703889@tec.mx>
2023-03-09 22:02:27 -0600

    fixRecordDesign

--- app/src/main/java/com/example/kotlin/robertoruizapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/robertoruizapp/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.kotlin.robertoruizapp
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.databinding.DataBindingUtil
 import com.example.kotlin.robertoruizapp.databinding.ActivityMainBinding
 
 class MainActivity: Activity() {
@@ -11,7 +14,23 @@ class MainActivity: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-    }
+        //binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
 
+        val spinnerSex = findViewById<Spinner>(R.id.spnSex)
+        val spinnerOcuppation = findViewById<Spinner>(R.id.spnOcuppation)
+        val spinnerEducation = findViewById<Spinner>(R.id.spnEducation)
+
+        val listaSex = listOf("Hombre", "Mujer", "Prefiero no decir")
+        val listaEducation = listOf("Ninguno", "Primaria", "Secundaria", "Preparatoria", "Universidad")
+        val listaOcuppation = listOf("Estudiante", "Trabajador", "Obrero", "Licuado", "Ingeniero")
+
+
+        val adaptadorSex = ArrayAdapter(this, android.R.layout.simple_spinner_item, listaSex)
+        spinnerSex.adapter = adaptadorSex
+        val adaptadorOccupation = ArrayAdapter(this, android.R.layout.simple_spinner_item, listaOcuppation)
+        spinnerOcuppation.adapter = adaptadorOccupation
+        val adaptadorEducation = ArrayAdapter(this, android.R.layout.simple_spinner_item, listaEducation)
+        spinnerEducation.adapter = adaptadorEducation
+    }
 
 }
