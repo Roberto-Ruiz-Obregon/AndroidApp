8ef4475ef0d8c4246aeed921b4dc3cb9aa6071ee
jorgeCastro-CVJJ <A01707735@itesm.mx>
2023-04-25 10:58:05 -0600

    Added Documentation to .kt file under domain package

--- app/src/main/java/com/example/kotlin/robertoruizapp/domain/ProgramInfoRequirement.kt
+++ app/src/main/java/com/example/kotlin/robertoruizapp/domain/ProgramInfoRequirement.kt
@@ -4,9 +4,21 @@ import com.example.kotlin.robertoruizapp.data.ProgramRepository
 import com.example.kotlin.robertoruizapp.data.network.model.program.Document
 import com.example.kotlin.robertoruizapp.data.network.model.program.Program
 
+/**
+ * ProgramInfoRequirement which has operator invoke method
+ *
+ */
 class ProgramInfoRequirement {
     private val repository = ProgramRepository()
 
+    /**
+     * Sets the function of the invoke operator as an asynchronous function
+     * that calls the getProgramInfo method from [ProgramRepository]
+     *
+     * @param numberProgram number also stated as the id of the program
+     *
+     * @return Program Type object
+     */
     suspend operator fun invoke(
         numberProgram: String
     ): Program? = repository.getProgramInfo(numberProgram)
--- app/src/main/java/com/example/kotlin/robertoruizapp/domain/ProgramListRequirement.kt
+++ app/src/main/java/com/example/kotlin/robertoruizapp/domain/ProgramListRequirement.kt
@@ -3,10 +3,23 @@ package com.example.kotlin.robertoruizapp.domain
 import com.example.kotlin.robertoruizapp.data.ProgramRepository
 import com.example.kotlin.robertoruizapp.data.network.model.program.Program
 
+/**
+ * ProgramListRequirement class which has operator invoke method
+ *
+ */
 class ProgramListRequirement {
 
     private val repository = ProgramRepository()
 
+    /**
+     *  Sets the function of the invoke operator as an asynchronous function
+     *  that calls the getProgramList method from [ProgramRepository]
+     *
+     * @param programName name of the program
+     * @param categorySelected category selected by the user
+     *
+     * @return Program Type object
+     */
     suspend operator fun invoke(
         programName: String, categorySelected: String
     ): Program? = repository.getProgramList(programName, categorySelected)
