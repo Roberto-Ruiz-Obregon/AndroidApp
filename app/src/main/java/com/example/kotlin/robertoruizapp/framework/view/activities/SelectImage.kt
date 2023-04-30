ebac88435a6f07bfaa15298faf656127d2e824d4
jorgeCastro-CVJJ <A01707735@itesm.mx>
2023-04-30 13:58:20 -0600

    added documentation to SelectImage

--- app/src/main/java/com/example/kotlin/robertoruizapp/framework/view/activities/SelectImage.kt
+++ app/src/main/java/com/example/kotlin/robertoruizapp/framework/view/activities/SelectImage.kt
@@ -7,7 +7,9 @@ import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlin.robertoruizapp.R
-
+/**
+ * SelectImage class that manages the activity actions
+ */
 class SelectImage: AppCompatActivity() {
 
     private lateinit var button: Button
@@ -16,7 +18,11 @@ class SelectImage: AppCompatActivity() {
     companion object {
         val IMAGE_REQUEST_CODE = 100
     }
-
+    /**
+     * Sets the information for the current fragment when creating the view
+     *
+     * @param savedInstanceState the state of the view
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragmento_forma_de_pago)
@@ -30,6 +36,9 @@ class SelectImage: AppCompatActivity() {
         }
     }
 
+    /**
+     * Opens the Selector Images of the photo gallery
+     */
     private fun pickImageGallery() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
@@ -37,6 +46,13 @@ class SelectImage: AppCompatActivity() {
 
     }
 
+    /**
+     * If the requestCode and result code are equal selected image is loaded
+     *
+     * @param requestCode request code of companion Object
+     * @param resultCode result code of the Activity
+     * @param data information of the Image
+     */
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK){
