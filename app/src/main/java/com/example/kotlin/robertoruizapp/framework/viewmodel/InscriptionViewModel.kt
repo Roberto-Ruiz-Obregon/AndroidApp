78243aa4428bb94ca2f0b4ed33e9483067debea6
jorgeCastro-CVJJ <A01707735@itesm.mx>
2023-04-30 22:12:32 -0600

    added documentation of InscriptionViewModel

--- app/src/main/java/com/example/kotlin/robertoruizapp/framework/viewmodel/InscriptionViewModel.kt
+++ app/src/main/java/com/example/kotlin/robertoruizapp/framework/viewmodel/InscriptionViewModel.kt
@@ -1,18 +1,20 @@
 package com.example.kotlin.robertoruizapp.framework.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.kotlin.robertoruizapp.data.network.model.ApiService
-import com.example.kotlin.robertoruizapp.data.network.model.NetworkModuleDI
 import com.example.kotlin.robertoruizapp.data.network.model.Inscripcion.Inscription
+import com.example.kotlin.robertoruizapp.data.network.model.NetworkModuleDI
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import timber.log.Timber
 
-
-class InscriptionViewModel: ViewModel() {
+/**
+ * InscriptionViewModel that manages the activity actions
+ *
+ */
+class InscriptionViewModel : ViewModel() {
 
     var InscripitionLiveData: MutableLiveData<Inscription?> = MutableLiveData()
 
@@ -20,17 +22,24 @@ class InscriptionViewModel: ViewModel() {
         return InscripitionLiveData
     }
 
-    fun enrollUser(token: String, course: Inscription){
+    /**
+     *  Calls the API service to POST the inscription of the user logged in the app
+     *  and manages the onFailure and onResponse methods
+     *
+     * @param token the user token session
+     * @param course the id of the course
+     */
+    fun enrollUser(token: String, course: Inscription) {
         val retroService = NetworkModuleDI.getRetroInstance().create(ApiService::class.java)
-        val call = retroService.postInscription(token,course)
-        call.enqueue(object: Callback<Inscription> {
+        val call = retroService.postInscription(token, course)
+        call.enqueue(object : Callback<Inscription> {
             override fun onFailure(call: Call<Inscription>, t: Throwable) {
-                Log.d("Falla de llamada", t.toString())
+
                 InscripitionLiveData.postValue(null)
             }
 
             override fun onResponse(call: Call<Inscription>, response: Response<Inscription>) {
-                if(response.isSuccessful) {
+                if (response.isSuccessful) {
                     Timber.tag("Salida").d(response.toString())
                     InscripitionLiveData.postValue(response.body())
                 } else {
